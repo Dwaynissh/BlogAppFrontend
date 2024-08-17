629292d8f041c233e02e49448a7f18e86c98f757
dwaynissh <dwaynissh@gmail.com>
2024-08-17 23:03:38 +0100

    .

--- src/Components/LayoutHolder/Layout.tsx
+++ src/Components/LayoutHolder/Layout.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Static/Header";
-import Footer from "../Static/Footer";
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
