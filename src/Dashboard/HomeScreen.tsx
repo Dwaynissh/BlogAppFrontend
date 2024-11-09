74d0db6535f332a5646150222113fe2474f20324
dwaynissh <dwaynissh@gmail.com>
2024-11-09 07:22:49 +0100

    search

--- src/Dashboard/HomeScreen.tsx
+++ src/Dashboard/HomeScreen.tsx
@@ -43,7 +43,7 @@ const HomeScreen = () => {
   };
 
   const filteredCards = data?.filter((el: any) => {
-    const titleName = `${el.title}`.toLowerCase();
+    const titleName = `${el.title} ${el.author}`.toLowerCase();
     return titleName.includes(searchCard.toLowerCase());
   });
 
